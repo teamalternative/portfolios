941546912d13a12646da5c8478fa29430356208c
RYOJI <baleonly@gmail.com>
2019-05-11 09:08:39 +0900

    プロジェクト

---
+++ components/molecules/ProjectThree.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="column">
+    <div class="card">
+      <div class="card-image">
+        <figure class="image is-4by3">
+          <img src="../../assets/images/project03.png" alt="project_01" />
+        </figure>
+      </div>
+      <div class="card-content">
+        <div class="content">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec
+          iaculis mauris.
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'nuxt-property-decorator'
+
+@Component
+export default class ProjectThree extends Vue {}
+</script>
