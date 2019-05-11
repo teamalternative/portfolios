6543b818e9a7d5c1bb2ca3da23eb9df6c4bfd152
RYOJI <baleonly@gmail.com>
2019-05-11 09:08:51 +0900

    LP

--- pages/index.vue
+++ pages/index.vue
@@ -1,67 +1,40 @@
 <template>
-  <section class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        portfolios
-      </h1>
-      <h2 class="subtitle">
-        My gnarly Nuxt.js project
-      </h2>
-      <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green"
-          >Documentation</a
-        >
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-          >GitHub</a
-        >
-      </div>
+  <div>
+    <div class="contain">
+      <FirstView />
+      <Projects />
+      <WhoWeAre />
+      <Contact />
+      <Footer />
     </div>
-  </section>
+  </div>
 </template>
 
-<script>
-import Logo from '~/components/Logo.vue'
-
-export default {
+<script lang="ts">
+import { Component, Vue } from 'nuxt-property-decorator'
+import Contact from '~/components/organisms/Contact.vue'
+import FirstView from '../components/organisms/FirstView.vue'
+import Projects from '../components/organisms/Projects.vue'
+import WhoWeAre from '../components/organisms/WhoWeAre.vue'
+import Footer from '../components/organisms/Footer.vue'
+@Component({
   components: {
-    Logo
+    Contact,
+    FirstView,
+    Projects,
+    WhoWeAre,
+    Footer
   }
-}
+})
+export default class extends Vue {}
 </script>
 
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
+<style lang="scss" scoped>
+.contain {
+  div {
+    min-height: 100vh;
+    justify-content: center;
+    align-items: center;
+  }
 }
 </style>
