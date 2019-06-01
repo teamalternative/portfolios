5064363b80ba17118729b694c2bd0ec90e3b2ffa
RYOJI <baleonly@gmail.com>
2019-06-01 14:04:00 +0900

    fix routing

---
+++ pages/projects/First.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <div>
+      <FirstView />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'nuxt-property-decorator'
+import FirstView from '../../components/organisms/FirstView.vue'
+
+@Component({
+  components: {
+    FirstView
+  }
+})
+export default class extends Vue {}
+</script>
